9e3981ed6e00fc623d4abc04279c70ce34a7a5cb
Administrator <Administrator@PC268>
2020-06-04 11:10:51 +0700

    Them lan 4

--- TuLeChiLAB_456/ViewModels/CourseViewModel.cs
+++ TuLeChiLAB_456/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using TuLeChiLAB_456.Models;
@@ -8,9 +9,13 @@ namespace TuLeChiLAB_456.ViewModels
 {
 	public class CourseViewModel
 	{
+        [Required]
         public string Place { get; set; }
+        [Required]
         public string Date { get; set; }
+        [Required]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
         public DateTime GetDateTime()
