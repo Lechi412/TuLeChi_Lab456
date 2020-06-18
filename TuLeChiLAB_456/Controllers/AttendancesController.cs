f99aad55ff57651a555eca5b9cd229764df1dbe4
Administrator <Administrator@PC249>
2020-06-18 10:11:00 +0700

    Them lan 10

---
+++ TuLeChiLAB_456/Controllers/AttendancesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuLeChiLAB_456.Models;
+
+namespace TuLeChiLAB_456.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeID == userId && a.CourseId == courseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeID = userId
+            };
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
+    }
+}
